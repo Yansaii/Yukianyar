91a82a1429017a8a43958a2544435a1febf30030
CyberXGithub <96681219+CyberXGithub@users.noreply.github.com>
2022-01-11 20:03:24 +0700

    Update __main__.py

--- Yukki/__main__.py
+++ Yukki/__main__.py
@@ -1,30 +1,61 @@
 import asyncio
 import importlib
-import os
-import re
 
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from pytgcalls import idle
 from rich.console import Console
-from rich.table import Table
 from youtubesearchpython import VideosSearch
-
-from config import (LOG_GROUP_ID, LOG_SESSION, STRING1, STRING2, STRING3,
-                    STRING4, STRING5)
-from Yukki import (ASS_CLI_1, ASS_CLI_2, ASS_CLI_3, ASS_CLI_4, ASS_CLI_5,
-                   ASSID1, ASSID2, ASSID3, ASSID4, ASSID5, ASSNAME1, ASSNAME2,
-                   ASSNAME3, ASSNAME4, ASSNAME5, BOT_ID, BOT_NAME, LOG_CLIENT,
-                   OWNER_ID, app)
+from Yukki import (
+    ASS_CLI_1,
+    ASS_CLI_2,
+    ASS_CLI_3,
+    ASS_CLI_4,
+    ASS_CLI_5,
+    ASSID1,
+    ASSID2,
+    ASSID3,
+    ASSID4,
+    ASSID5,
+    ASSNAME1,
+    ASSNAME2,
+    ASSNAME3,
+    ASSNAME4,
+    ASSNAME5,
+    BOT_ID,
+    BOT_NAME,
+    LOG_CLIENT,
+    OWNER_ID,
+    app,
+)
 from Yukki.Core.Clients.cli import LOG_CLIENT
-from Yukki.Core.PyTgCalls.Yukki import (pytgcalls1, pytgcalls2, pytgcalls3,
-                                        pytgcalls4, pytgcalls5)
-from Yukki.Database import (get_active_chats, get_active_video_chats,
-                            get_sudoers, is_on_off, remove_active_chat,
-                            remove_active_video_chat)
+from Yukki.Core.PyTgCalls.Yukki import (
+    pytgcalls1,
+    pytgcalls2,
+    pytgcalls3,
+    pytgcalls4,
+    pytgcalls5,
+)
+from Yukki.Database import (
+    get_active_chats,
+    get_active_video_chats,
+    get_sudoers,
+    is_on_off,
+    remove_active_chat,
+    remove_active_video_chat,
+)
 from Yukki.Inline import private_panel
 from Yukki.Plugins import ALL_MODULES
-from Yukki.Utilities.inline import paginate_modules
+
+from config import (
+    LOG_GROUP_ID,
+    LOG_SESSION,
+    STRING1,
+    STRING2,
+    STRING3,
+    STRING4,
+    STRING5,
+)
 
 loop = asyncio.get_event_loop()
 console = Console()
@@ -40,18 +71,16 @@ async def initiate_bot():
             for chat in chats:
                 chat_id = int(chat["chat_id"])
                 await remove_active_video_chat(chat_id)
-        except Exception as e:
+        except Exception:
             pass
         try:
             chats = await get_active_chats()
             for chat in chats:
                 chat_id = int(chat["chat_id"])
                 await remove_active_chat(chat_id)
-        except Exception as e:
+        except Exception:
             pass
-        status.update(
-            status="[bold blue]Scanning for Plugins", spinner="earth"
-        )
+        status.update(status="[bold blue]Scanning for Plugins", spinner="earth")
         console.print("Found {} Plugins".format(len(ALL_MODULES)) + "\n")
         status.update(
             status="[bold red]Importing Plugins...",
@@ -59,21 +88,11 @@ async def initiate_bot():
             spinner_style="yellow",
         )
         for all_module in ALL_MODULES:
-            imported_module = importlib.import_module(
-                "Yukki.Plugins." + all_module
-            )
-            if (
-                hasattr(imported_module, "__MODULE__")
-                and imported_module.__MODULE__
-            ):
+            imported_module = importlib.import_module("Yukki.Plugins." + all_module)
+            if hasattr(imported_module, "__MODULE__") and imported_module.__MODULE__:
                 imported_module.__MODULE__ = imported_module.__MODULE__
-                if (
-                    hasattr(imported_module, "__HELP__")
-                    and imported_module.__HELP__
-                ):
-                    HELPABLE[
-                        imported_module.__MODULE__.lower()
-                    ] = imported_module
+                if hasattr(imported_module, "__HELP__") and imported_module.__HELP__:
+                    HELPABLE[imported_module.__MODULE__.lower()] = imported_module
             console.print(
                 f">> [bold cyan]Successfully imported: [green]{all_module}.py"
             )
@@ -81,15 +100,13 @@ async def initiate_bot():
         status.update(
             status="[bold blue]Importation Completed!",
         )
-    console.print(
-        "[bold green]Congrats!! Yukki Music Bot has started successfully!\n"
-    )
+    console.print("[bold green]Congrats!! Yukki Music Bot has started successfully!\n")
     try:
         await app.send_message(
             LOG_GROUP_ID,
             "<b>Congrats!! Music Bot has started successfully!</b>",
         )
-    except Exception as e:
+    except Exception:
         print(
             "\nBot has failed to access the log Channel. Make sure that you have added your bot to your log channel and promoted as admin!"
         )
@@ -108,7 +125,7 @@ async def initiate_bot():
                 LOG_GROUP_ID,
                 "<b>Congrats!! Assistant Client 1  has started successfully!</b>",
             )
-        except Exception as e:
+        except Exception:
             print(
                 "\nAssistant Account 1 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
             )
@@ -117,7 +134,7 @@ async def initiate_bot():
         try:
             await ASS_CLI_1.join_chat("OfficialYukki")
             await ASS_CLI_1.join_chat("YukkiSupport")
-        except:
+        except BaseException:
             pass
         console.print(f"├[red] Assistant 1 Started as {ASSNAME1}!")
         console.print(f"├[green] ID :- {ASSID1}!")
@@ -127,7 +144,7 @@ async def initiate_bot():
                 LOG_GROUP_ID,
                 "<b>Congrats!! Assistant Client 2 has started successfully!</b>",
             )
-        except Exception as e:
+        except Exception:
             print(
                 "\nAssistant Account 2 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
             )
@@ -136,7 +153,7 @@ async def initiate_bot():
         try:
             await ASS_CLI_2.join_chat("OfficialYukki")
             await ASS_CLI_2.join_chat("YukkiSupport")
-        except:
+        except BaseException:
             pass
         console.print(f"├[red] Assistant 2 Started as {ASSNAME2}!")
         console.print(f"├[green] ID :- {ASSID2}!")
@@ -146,7 +163,7 @@ async def initiate_bot():
                 LOG_GROUP_ID,
                 "<b>Congrats!! Assistant Client 3 has started successfully!</b>",
             )
-        except Exception as e:
+        except Exception:
             print(
                 "\nAssistant Account 3 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
             )
@@ -155,7 +172,7 @@ async def initiate_bot():
         try:
             await ASS_CLI_3.join_chat("OfficialYukki")
             await ASS_CLI_3.join_chat("YukkiSupport")
-        except:
+        except BaseException:
             pass
         console.print(f"├[red] Assistant 3 Started as {ASSNAME3}!")
         console.print(f"├[green] ID :- {ASSID3}!")
@@ -165,7 +182,7 @@ async def initiate_bot():
                 LOG_GROUP_ID,
                 "<b>Congrats!! Assistant Client 4 has started successfully!</b>",
             )
-        except Exception as e:
+        except Exception:
             print(
                 "\nAssistant Account 4 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
             )
@@ -174,7 +191,7 @@ async def initiate_bot():
         try:
             await ASS_CLI_4.join_chat("OfficialYukki")
             await ASS_CLI_4.join_chat("YukkiSupport")
-        except:
+        except BaseException:
             pass
         console.print(f"├[red] Assistant 4 Started as {ASSNAME4}!")
         console.print(f"├[green] ID :- {ASSID4}!")
@@ -184,7 +201,7 @@ async def initiate_bot():
                 LOG_GROUP_ID,
                 "<b>Congrats!! Assistant Client 5 has started successfully!</b>",
             )
-        except Exception as e:
+        except Exception:
             print(
                 "\nAssistant Account 5 has failed to access the log Channel. Make sure that you have added your Assistant to your log channel and promoted as admin!"
             )
@@ -193,7 +210,7 @@ async def initiate_bot():
         try:
             await ASS_CLI_5.join_chat("OfficialYukki")
             await ASS_CLI_5.join_chat("YukkiSupport")
-        except:
+        except BaseException:
             pass
         console.print(f"├[red] Assistant 5 Started as {ASSNAME5}!")
         console.print(f"├[green] ID :- {ASSID5}!")
@@ -203,7 +220,7 @@ async def initiate_bot():
                 LOG_GROUP_ID,
                 "<b>Congrats!! Logger Client has started successfully!</b>",
             )
-        except Exception as e:
+        except Exception:
             print(
                 "\nLogger Client has failed to access the log Channel. Make sure that you have added your Logger Account to your log channel and promoted as admin!"
             )
@@ -212,7 +229,7 @@ async def initiate_bot():
         try:
             await LOG_CLIENT.join_chat("OfficialYukki")
             await LOG_CLIENT.join_chat("YukkiSupport")
-        except:
+        except BaseException:
             pass
     console.print(f"└[red] Yukki Music Bot Boot Completed.")
     if STRING1 != "None":
@@ -229,19 +246,6 @@ async def initiate_bot():
     console.print(f"\n[red]Stopping Bot")
 
 
-home_text_pm = f"""Hello ,
-My name is {BOT_NAME}.
-A Telegram Music+Video Streaming bot with some useful features.
-
-All commands can be used with: / """
-
-
-@app.on_message(filters.command("help") & filters.private)
-async def help_command(_, message):
-    text, keyboard = await help_parser(message.from_user.mention)
-    await app.send_message(message.chat.id, text, reply_markup=keyboard)
-
-
 @app.on_message(filters.command("start") & filters.private)
 async def start_command(_, message):
     if len(message.text.split()) > 1:
@@ -253,9 +257,7 @@ async def start_command(_, message):
             for x in OWNER_ID:
                 try:
                     user = await app.get_users(x)
-                    user = (
-                        user.first_name if not user.mention else user.mention
-                    )
+                    user = user.first_name if not user.mention else user.mention
                     sex += 1
                 except Exception:
                     continue
@@ -265,11 +267,7 @@ async def start_command(_, message):
                 if user_id not in OWNER_ID:
                     try:
                         user = await app.get_users(user_id)
-                        user = (
-                            user.first_name
-                            if not user.mention
-                            else user.mention
-                        )
+                        user = user.first_name if not user.mention else user.mention
                         if smex == 0:
                             smex += 1
                             text += "\n⭐️<u> **Sudo Users:**</u>\n"
@@ -330,9 +328,7 @@ async def start_command(_, message):
                         InlineKeyboardButton(
                             text="🎥 Watch Youtube Video", url=f"{link}"
                         ),
-                        InlineKeyboardButton(
-                            text="🔄 Close", callback_data="close"
-                        ),
+                        InlineKeyboardButton(text="🔄 Close", callback_data="close"),
                     ],
                 ]
             )
@@ -367,116 +363,6 @@ async def start_command(_, message):
             f"{message.from_user.mention} has just started Bot.\n\n**USER ID:** {sender_id}\n**USER NAME:** {sender_name}",
         )
     return
-
-
-async def help_parser(name, keyboard=None):
-    if not keyboard:
-        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
-    return (
-        """Hello {first_name},
-
-Click on the buttons for more information.
-
-All commands can be used with: /
-""".format(
-            first_name=name
-        ),
-        keyboard,
-    )
-
-
-@app.on_callback_query(filters.regex("shikhar"))
-async def shikhar(_, CallbackQuery):
-    text, keyboard = await help_parser(CallbackQuery.from_user.mention)
-    await CallbackQuery.message.edit(text, reply_markup=keyboard)
-
-
-@app.on_callback_query(filters.regex(r"help_(.*?)"))
-async def help_button(client, query):
-    home_match = re.match(r"help_home\((.+?)\)", query.data)
-    mod_match = re.match(r"help_module\((.+?)\)", query.data)
-    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
-    next_match = re.match(r"help_next\((.+?)\)", query.data)
-    back_match = re.match(r"help_back", query.data)
-    create_match = re.match(r"help_create", query.data)
-    top_text = f"""Hello {query.from_user.first_name},
-
-Click on the buttons for more information.
-
-All commands can be used with: /
- """
-    if mod_match:
-        module = mod_match.group(1)
-        text = (
-            "{} **{}**:\n".format(
-                "Here is the help for", HELPABLE[module].__MODULE__
-            )
-            + HELPABLE[module].__HELP__
-        )
-        key = InlineKeyboardMarkup(
-            [
-                [
-                    InlineKeyboardButton(
-                        text="↪️ Back", callback_data="help_back"
-                    ),
-                    InlineKeyboardButton(
-                        text="🔄 Close", callback_data="close"
-                    ),
-                ],
-            ]
-        )
-
-        await query.message.edit(
-            text=text,
-            reply_markup=key,
-            disable_web_page_preview=True,
-        )
-    elif home_match:
-        out = private_panel()
-        await app.send_message(
-            query.from_user.id,
-            text=home_text_pm,
-            reply_markup=InlineKeyboardMarkup(out[1]),
-        )
-        await query.message.delete()
-    elif prev_match:
-        curr_page = int(prev_match.group(1))
-        await query.message.edit(
-            text=top_text,
-            reply_markup=InlineKeyboardMarkup(
-                paginate_modules(curr_page - 1, HELPABLE, "help")
-            ),
-            disable_web_page_preview=True,
-        )
-
-    elif next_match:
-        next_page = int(next_match.group(1))
-        await query.message.edit(
-            text=top_text,
-            reply_markup=InlineKeyboardMarkup(
-                paginate_modules(next_page + 1, HELPABLE, "help")
-            ),
-            disable_web_page_preview=True,
-        )
-
-    elif back_match:
-        await query.message.edit(
-            text=top_text,
-            reply_markup=InlineKeyboardMarkup(
-                paginate_modules(0, HELPABLE, "help")
-            ),
-            disable_web_page_preview=True,
-        )
-
-    elif create_match:
-        text, keyboard = await help_parser(query)
-        await query.message.edit(
-            text=text,
-            reply_markup=keyboard,
-            disable_web_page_preview=True,
-        )
-
-    return await client.answer_callback_query(query.id)
 
 
 if __name__ == "__main__":
